9e3380d7e01d0bf697f5cfc0d419007bc56a7f74
Luis <Luis@DESKTOP-TDGOJHH>
2022-05-23 08:36:30 -0800

    Dashboard en angular

---
+++ angular/adminpro/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PagenotfoundComponent,
+    DashboardComponent,
+    BreadcrumsComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
